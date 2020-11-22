551befc19dfa99af29d1aa970d98fc8b1bb44cbf
Henry Pérez <henry.perez2017@uteq.edu.ec>
2020-11-22 16:01:04 -0500

    Modificación de la ventana de bienvenida, a un mensaje más informativo

--- app/src/main/java/com/example/miapp/actBienvenida.java
+++ app/src/main/java/com/example/miapp/actBienvenida.java
@@ -13,20 +13,23 @@ public class actBienvenida extends AppCompatActivity {
         setContentView(R.layout.activity_act_bienvenida);
 
         //Localizar los controles
-        TextView nombre, genero, f_nacimiento, telefono;
-        nombre = (TextView)findViewById(R.id.txtRNombre);
-        f_nacimiento = (TextView)findViewById(R.id.txtRFNac);
-        telefono = (TextView)findViewById(R.id.txtRTelefono);
-        genero = (TextView)findViewById(R.id.txtRGenero);
+        TextView mensaje;
+        mensaje = findViewById(R.id.txtRMensaje);
 
         //Recuperamos la información pasada en el intent
         Bundle bundel = this.getIntent().getExtras();
 
         //Construimos el mensaje a mostrar
-        nombre.setText(bundel.getString("nombre"));
-        f_nacimiento.setText(bundel.getString("f_nacimiento"));
-        telefono.setText(bundel.getString("telefono"));
-        genero.setText(bundel.getString("genero"));
+        String cadena;
+
+        if (bundel.getString("genero").equals("Masculino"))
+            cadena = "Hola, bienvenido " + bundel.getString("nombre");
+        else
+            cadena = "Hola, bienvenida " + bundel.getString("nombre");
+
+        cadena += " su fecha de nacimiento es " + bundel.getString("f_nacimiento")
+                + ", y su número de télefono es: " + bundel.getString("telefono") + " y lo registramos para contactarnos en un futuro próximo. \n\nGracias por su atención y confiar en nosotros.";
+        mensaje.setText(cadena);
 
     }
 }
