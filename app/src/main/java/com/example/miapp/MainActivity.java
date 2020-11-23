b84e0f82397ce26812399ab39794c5c1b4a38ce5
Henry Pérez <henry.perez2017@uteq.edu.ec>
2020-11-22 19:44:56 -0500

    Validación de envio de datos mejorada

--- app/src/main/java/com/example/miapp/MainActivity.java
+++ app/src/main/java/com/example/miapp/MainActivity.java
@@ -28,12 +28,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Asignamos los valores a las referencias
-        nombre = (EditText)findViewById(R.id.edNombre);
-        f_nacimiento = (EditText)findViewById(R.id.edFechaN);
-        telefono = (EditText)findViewById(R.id.edTelefono);
+        nombre = findViewById(R.id.edNombre);
+        f_nacimiento = findViewById(R.id.edFechaN);
+        telefono = findViewById(R.id.edTelefono);
 
-        rMasculino = (RadioButton)findViewById(R.id.rbtMasculino);
-        rFemenino = (RadioButton)findViewById(R.id.rbtFemenino);
+        rMasculino = findViewById(R.id.rbtMasculino);
+        rFemenino = findViewById(R.id.rbtFemenino);
 
     }
 
@@ -68,11 +68,9 @@ public class MainActivity extends AppCompatActivity {
         mes = calendar.get(MONTH);
         ano = calendar.get(YEAR);
 
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                f_nacimiento.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
-            }
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view1, year, month, dayOfMonth) -> {
+            String fecha = dayOfMonth + "/" + (month + 1) + "/" + year;
+            f_nacimiento.setText(fecha);
         }, ano, mes, dia);
         datePickerDialog.show();
 
@@ -80,9 +78,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick_Enviar(View view)
     {
-        if(nombre.getText().toString() != "" && f_nacimiento.getText().toString() != "" && (rFemenino.isChecked() || rMasculino.isChecked()) && telefono.getText().toString() != "" && telefono.getText().length() == 10)
-            Envio();
-        else
+        if(nombre.getText().toString().equals("") || f_nacimiento.getText().toString().equals("") || (!rFemenino.isChecked() && !rMasculino.isChecked()) || telefono.getText().toString().equals(""))
             Toast.makeText(getApplicationContext(), "Todo los campos son obligatorio", Toast.LENGTH_SHORT).show();
+        else if(telefono.getText().length() != 10)
+            Toast.makeText(getApplicationContext(), "La cantidad de dígitos que debe colocar en su télefono es 10", Toast.LENGTH_SHORT).show();
+        else
+            Envio();
     }
 }
